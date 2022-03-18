84b8bee8e6714053eb4c6d16b387dc752d01ed0e
Mohamed <mohamed.benahmed@etudiant-fsegt.utm.tn>
2022-03-18 17:03:41 +0100

    user/security fix

---
+++ src/Controller/UserController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+/**
+ * @Route("/api")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user", name="app_user")
+     */
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    /**
+     * @Route(
+     *     name="api_users_post",
+     *     path="/users",
+     *     methods={"POST"},
+     *     defaults={
+     *         "_api_resource_class"=User::class,
+     *         "_api_collection_operation_name"="post"
+     *     }
+     * )
+     */
+    public function postAction(User $data, UserPasswordEncoderInterface $encoder): User
+    {
+        return $this->encodePassword($data, $encoder);
+    }
+
+    /**
+     * @Route(
+     *     name="api_users_put",
+     *     path="/users/{id}",
+     *     requirements={"id"="\d+"},
+     *     methods={"PUT"},
+     *     defaults={
+     *         "_api_resource_class"=User::class,
+     *         "_api_item_operation_name"="put"
+     *     }
+     * )
+     */
+    public function putAction(User $data, UserPasswordEncoderInterface $encoder): User
+    {
+        return $this->encodePassword($data, $encoder);
+    }
+   
+
+    /**
+     * @Route(
+     *     name="api_users_patch",
+     *     path="/users/{id}",
+     *     requirements={"id"="\d+"},
+     *     methods={"PATCH"},
+     *     defaults={
+     *         "_api_resource_class"=User::class,
+     *         "_api_item_operation_name"="patch"
+     *     }
+     * )
+     */
+    public function patchAction(User $data, UserPasswordEncoderInterface $encoder): User
+    {
+        return $this->encodePassword($data, $encoder);
+    }
+
+    protected function encodePassword(User $data, UserPasswordEncoderInterface $encoder): User
+    {
+        $encoded = $encoder->encodePassword($data, $data->getPassword());
+        $data->setPassword($encoded);
+
+        return $data;
+    }
+
+    public function getTokenUser(UserInterface $user, JWTTokenManagerInterface $JWTManager)
+    {
+        // ...
+
+        return new JsonResponse(['token' => $JWTManager->create($user)]);
+    }
+
+
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,15 +2,14 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 /**
- * @ApiResource()
  * @ORM\Entity(repositoryClass=UserRepository::class)
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id
@@ -20,12 +19,18 @@ class User
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=180, unique=true)
      */
     private $username;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
      */
     private $password;
 
@@ -54,9 +59,14 @@ class User
         return $this->id;
     }
 
-    public function getUsername(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
     {
-        return $this->username;
+        return (string) $this->username;
     }
 
     public function setUsername(string $username): self
@@ -66,7 +76,29 @@ class User
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
     {
         return $this->password;
     }
@@ -76,6 +108,26 @@ class User
         $this->password = $password;
 
         return $this;
+    }
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 
     public function getNom(): ?string
